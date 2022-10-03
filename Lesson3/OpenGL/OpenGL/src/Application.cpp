6ef3863fda1b1006a207067397fc07ad6fde740e
cjh8817676 <jonathan1446171@gmail.com>
2022-10-03 12:04:14 +0800

    chapter 3

---
+++ Lesson3/OpenGL/OpenGL/src/Application.cpp
@@ -0,0 +1,64 @@
+#include <GL/glew.h>          // glew 一定叫在 opengl(GLFW) 前面。
+#include <GLFW/glfw3.h>
+#include <iostream>
+int main(void)
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK)
+		std::cout << "Error!" << std::endl;
+
+	std::cout << glGetString(GL_VERSION) << std::endl; // 讀取GPU Driver版本。
+
+	float positions[6] = {
+		-0.5f, -0.5f,
+		0.0f, 0.5f,
+		0.5f, -0.5f
+	};
+
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+	 
+	//glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glDrawArrays(GL_TRIANGLES, 0, 1);
+		glDrawArrays(GL_TRIANGLES, 0, 2);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// glDrawElements(GL_TRIANGLES, 3, float);
+
+
+		//glDrawElements(GL_TRIANGLES);
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
--- Readme.md
+++ Readme.md
@@ -1,2 +1,3 @@
 Lesson1: Setting and GLFW
 Lesson2: GLEW Initialize
+Lesson3: Vertex Buffers and deawing a triangle
